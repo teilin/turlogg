0a8cb6319565127e7d43a33c131be9c4f4ac18ac
Teis Lindemark <teis.lindemark@gmail.com>
2016-06-30 16:39:35 +0200

    Minor changes

---
+++ src/Turlogg/AppSettings.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace WebApplication
+{
+  public class AppSettings
+  {
+    public string NasjonalTurbaseApiKey { get; set; }
+  }
+}
--- src/Turlogg/Startup.cs
+++ src/Turlogg/Startup.cs
@@ -40,6 +40,8 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<AppSettings>(options => Configuration.GetSection("AppSettings").Bind(options));
+
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
